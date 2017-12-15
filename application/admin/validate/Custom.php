cb4f0e55700786bf63156163e37ca3f97d6ca876
dohaifeng <13977989372@163.com>
2017-12-15 15:36:42 +0800

    Rejected commit: Default

--- application/admin/validate/Custom.php
+++ application/admin/validate/Custom.php
@@ -12,5 +12,13 @@ class custom extends Validate
 {
     protected $rule = [
         ['certificate','require','证书号必须填写'],
+        ['shape_id','require','形状必须选择'],
+        ['color','require','颜色必须选择'],
+        ['neatness','require','净度必须选择'],
+        ['cut','require','切工必须选择'],
+        ['polishing','require','抛光必须选择'],
+        ['symmetry','require','对称必须选择'],
+        ['weight','require','重量必须填写'],
+        ['price','require','价格必须填写'],
     ];
 }
